8945ef5312aef389591f8e928c09eabce4f63958
heliosdrm <helios.derosario@gmail.com>
2016-05-06 14:39:45 +0200

    Different types of entropies (Humeau-Heurtier 2015)
    MSE, MMSE, CMSE, RCMSE

--- src/MultiScaleEntropy.jl
+++ src/MultiScaleEntropy.jl
@@ -6,39 +6,80 @@ function approximateentropy(x, m, r)
     xe_plus = embed(x, m+1, 1)
     xe = [xe_plus[:,1:end-1]; xe_plus[end,2:end]]
     n = size(xe)[1]
-    rmat = recurrencematrix(xe, r)
+    rmat = recurrencematrix(xe, std(xe)*r)
     c = sum(rmat, 1)/n
     apen = sum(log(c))/n
-    rmat = recurrencematrix(xe_plus, r)
+    rmat = recurrencematrix(xe_plus, std(xe_plus)*r)
     c = sum(rmat, 1)/(n-1)
     apen_plus = sum(log(c))/(n-1)
     apen - apen_plus
 end
 
-function sampleentropy(x, m, r)
-    xe_plus = embed(x, m+1, 1)
-    xe = [xe_plus[:,1:end-1]; xe_plus[end,2:end]]
+function sampleentropy_nd(x, m, r, delay=1)
+    xe_plus = embed(x, m+1, delay)
+    xe = [xe_plus[:,1:end-1]; xe_plus[end-delay+1:end,2:end]]
     n = size(xe)[1]
-    rmat = recurrencematrix(xe, r)
+    rmat = recurrencematrix(xe, std(xe)*r)
     b = recurrencerate(rmat, theiler=1)*n*(n-1)
-    rmat = recurrencematrix(xe_plus, r)
-    a = recurrencerate(rmat, theiler=1)*(n-1)*(n-2)
+    rmat = recurrencematrix(xe_plus, std(xe_plus)*r)
+    a = recurrencerate(rmat, theiler=1)*(n-delay)*(n-delay-1)
+    (a, b)
+end
+
+function sampleentropy(args...)
+    a, b = sampleentropy_nd(args...)
     -log(a/b)
 end
 
+
+
 # MSE with coarse-grained series
-# CompMSE with moving average
+# MMSE with moving average
 
 moving_average(x, ns) = filt(ones(ns)/ns, 1, x)[ns:end]
 grained(x, ns) = moving_average(x, ns)[1:ns:end]
 
-function mse(x, m, r, bounds; ent_type="ApEn", scale="grained")
-    ent_fun = Dict("ApEn"=>approximateentropy, "SampEn"=>sampleentropy)
-    scale_fun = Dict("grained"=>grained, "moving"=>moving_average)
+function mse(x, m, r, bounds; ent_type="SampEn")
+    ent_fun = Dict("ApEn"=>approximateentropy, "SampEn"=>sampleentropy)[ent_type]
     ent = zeros(bounds[2]-bounds[1]+1)
     for s in range(bounds...)
-        xs = scale_fun(x, s)
-        ent[s] = scale_fun(xs, m, r)
+        xs = grained(x, s)
+        ent[s] = ent_fun(xs, m, r)
+    end
+    ent
+end
+
+function mmse(x, m, r, bounds)
+    ent = zeros(bounds[2]-bounds[1]+1)
+    for s in range(bounds...)
+        xs = moving_average(x, s)
+        ent[s] = sampleentropy(xs, m, r, s)
+    end
+    ent
+end
+
+function cmse(x, m, r, bounds)
+    ent = zeros(bounds[2]-bounds[1]+1)
+    for s in range(bounds...)
+        for k = 1:s
+            xs = grained(x[k:end], s)
+            ent[s] += sampleentropy(xs, m, r) / s
+        end
+    end
+    ent
+end
+
+function rcmse(x, m, r, bounds)
+    ent = zeros(bounds[2]-bounds[1]+1)
+    for s in range(bounds...)
+        a = b = 0.
+        for k = 1:s
+            xs = grained(x[k:end], s)
+            ak, bk = sampleentropy_nd(xs, m, r)
+            a += ak
+            b += bk
+        end
+        ent[s] = -log(a/b)
     end
     ent
 end
